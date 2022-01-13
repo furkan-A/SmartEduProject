8c956ece926570703c98ef546b16708e8a3338cb
furkan-A <furkanakt21@gmail.com>
2022-01-14 02:55:36 +0300

    add and organize dashboard redirect

--- controller/authController.js
+++ controller/authController.js
@@ -26,7 +26,7 @@ exports.loginUser = async (req, res) => {
             bcrypt.compare(password, user.password, (err, same) => {
                 if (same) {
                     req.session.userID = user._id;
-                    res.status(200).redirect('/');
+                    res.status(200).redirect('/users/dashboard');
                 }
             });
         }
@@ -41,5 +41,13 @@ exports.loginUser = async (req, res) => {
 exports.logOutUser = async (req, res) => {
     req.session.destroy(() => {
         res.redirect('/');
+    });
+};
+
+exports.getDashboardPage = async (req, res) => {
+    const user = await User.findOne({_id: req.session.userID});
+    res.status(200).render("dashboard", {
+        page_name: "dashboard",
+        user
     });
 };
